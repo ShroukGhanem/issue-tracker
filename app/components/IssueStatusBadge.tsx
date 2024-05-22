ed00e6b62c097cdb9eaac2ca8a275317ca02fdc5
Shrouk Ghanem <Shrouk.Ghanem@integrant.com>
2024-05-22 12:47:27 +0300

    built the issue status badge

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,24 @@
+import { Status } from "@prisma/client";
+import { Badge } from "@radix-ui/themes";
+import React from "react";
+
+interface Props {
+  status: Status;
+}
+
+const statusMap: Record<
+  Status,
+  { label: string; color: "red" | "violet" | "green" }
+> = {
+  OPEN: { label: "OPEN", color: "red" },
+  IN_PROGRESS: { label: "IN PROGRESS", color: "violet" },
+  CLOSED: { label: "CLOSED", color: "green" },
+};
+
+const IssueStatusBadge = ({ status }: Props) => {
+  return (
+    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+  );
+};
+
+export default IssueStatusBadge;
