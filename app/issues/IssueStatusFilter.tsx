ee6013b2d2e81c49597c9bfd44b040e538082da4
Shrouk Ghanem <Shrouk.Ghanem@integrant.com>
2024-05-28 15:23:55 +0300

    Filtering issues by status

--- app/issues/IssueStatusFilter.tsx
+++ app/issues/IssueStatusFilter.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const statuses: { label: String; value?: Status }[] = [
@@ -11,8 +12,19 @@ const statuses: { label: String; value?: Status }[] = [
 ];
 
 const IssueStatusFilter = () => {
+  const router = useRouter();
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(status) => {
+        const parameter = status
+          ? status === "all"
+            ? ""
+            : `?status=${status}`
+          : "";
+
+        router.push("/issues" + parameter);
+      }}
+    >
       <Select.Trigger placeholder="Filter by status" />
       <Select.Content>
         {statuses.map((status) => (
