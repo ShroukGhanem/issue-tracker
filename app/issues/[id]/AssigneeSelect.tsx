71b9d5b6ce4a696501dd3eb9535daba7ce4355c0
Shrouk Ghanem <Shrouk.Ghanem@integrant.com>
2024-05-28 13:07:30 +0300

    fetching users with reactquery

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,27 +1,41 @@
 "use client";
 import { User } from "@prisma/client";
-import { Select } from "@radix-ui/themes";
+import { Callout, Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { Skeleton } from "@/app/components";
+import { BiErrorCircle } from "react-icons/bi";
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("x/api/users").then((response) => response.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
+  if (isLoading) return <Skeleton />;
+  if (error)
+    return (
+      <Callout.Root className="mb-5" color="red" size="1" variant="outline">
+        <Callout.Icon>
+          <BiErrorCircle />
+        </Callout.Icon>
+        <Callout.Text>Error Loading Assignees</Callout.Text>
+      </Callout.Root>
+    );
 
-    fetchUsers();
-  }, []);
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..."></Select.Trigger>
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
